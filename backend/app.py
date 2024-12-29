209f7c2fedcc5104aa8ceb37b77580fa26d629e2
Bogdan Bescherepov <bogdanrus@list.ru>
2024-12-30 01:20:02 +0300

    flask app test data little format change add CORS so client app can get info from server

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 MATCHES = [
     {
+        'tournament': 'Tournament #1',
         'team1': 'Red',
         'team2': 'Blue',
-        'score': '2:0'
+        'team1_score': '2',
+        'team2_score': '0'
     }
 ]
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 @app.route('/matches', methods=['GET'])
 def all_matches():
